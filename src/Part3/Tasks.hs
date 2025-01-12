b3054d7b34e149aa09d26b611e1e956da03e887f
Vladislav Kalugin <kalugin.vlad@gmail.com>
2025-01-13 01:52:58 +0300

    task3

--- src/Part3/Tasks.hs
+++ src/Part3/Tasks.hs
@@ -1,25 +1,31 @@
 module Part3.Tasks where
 
 import Util (notImplementedYet)
+import Data.List(sort, group, maximumBy)
+import Data.Ord(comparing)
 
 -- Функция finc принимает на вход функцию f и число n и возвращает список чисел [f(n), f(n + 1), ...]
 finc :: (Int -> a) -> Int -> [a]
-finc = notImplementedYet
+finc f n = [f x | x <- [n..]]
 
 -- Функция ff принимает на вход функцию f и элемент x и возвращает список [x, f(x), f(f(x)), f(f(f(x))) ...]
 ff :: (a -> a) -> a -> [a]
-ff = notImplementedYet
+ff f x = x : ff f (f x)
 
 -- Дан список чисел. Вернуть самую часто встречающуюся *цифру* в этих числах (если таковых несколько -- вернуть любую)
 mostFreq :: [Int] -> Int
-mostFreq = notImplementedYet
+mostFreq numbers = read [head $ maximumBy (comparing length) digits]
+    where
+        digits = group $ sort $ concatMap show [abs x | x <- numbers]
 
 -- Дан список lst. Вернуть список элементов из lst без повторений, порядок может быть произвольным.
 uniq :: (Eq a) => [a] -> [a]
-uniq = notImplementedYet
+uniq [] = []
+uniq (h : t) | h `elem` t = uniq t
+             | otherwise = h : uniq t
 
 -- Функция grokBy принимает на вход список Lst и функцию F и каждому возможному
 -- значению результата применения F к элементам Lst ставит в соответствие список элементов Lst,
 -- приводящих к этому результату. Результат следует представить в виде списка пар.
 grokBy :: (Eq k) => (a -> k) -> [a] -> [(k, [a])]
-grokBy f l = notImplementedYet
+grokBy f l = [(key, [x | x <- l, f x == key]) | key <- uniq (map f l)]
