e69924f5cc0438aa064e49e9b7c90e04b52337c2
Vladislav Kalugin <kalugin.vlad@gmail.com>
2025-01-13 03:55:54 +0300

    Task5

--- src/Part5/Tasks.hs
+++ src/Part5/Tasks.hs
@@ -4,29 +4,30 @@ import Util(notImplementedYet)
 
 -- Реализуйте левую свёртку
 myFoldl :: (b -> a -> b) -> b -> [a] -> b
-myFoldl = notImplementedYet
+myFoldl f b [] = b
+myFoldl f b (fr : t) = myFoldl f (f b fr) t
 
 -- Реализуйте правую свёртку
 myFoldr :: (a -> b -> b) -> b -> [a] -> b
-myFoldr = notImplementedYet
+myFoldr f b [] = b
+myFoldr f b (fr : t) = f fr (myFoldr f b t)
 
 -- Используя реализации свёрток выше, реализуйте все остальные функции в данном файле
 
 myMap :: (a -> b) -> [a] -> [b]
-myMap f = notImplementedYet
+myMap f = myFoldl (\fr nx -> fr ++ [f nx]) []
 
 myConcatMap :: (a -> [b]) -> [a] -> [b]
-myConcatMap f = notImplementedYet
+myConcatMap f = myFoldl (\fr nx -> fr ++ f nx) []
 
 myConcat :: [[a]] -> [a]
-myConcat = notImplementedYet
+myConcat = myFoldl (++) []
 
 myReverse :: [a] -> [a]
-myReverse = notImplementedYet
+myReverse = myFoldl (\fr nx -> nx : fr) []
 
 myFilter :: (a -> Bool) -> [a] -> [a]
-myFilter p = notImplementedYet
+myFilter p = myFoldl (\fr nx -> if p nx then fr ++ [nx] else fr) []
 
 myPartition :: (a -> Bool) -> [a] -> ([a], [a])
-myPartition p = notImplementedYet
-
+myPartition p = myFoldl (\(fr, fr2) nx -> if p nx then (fr ++ [nx], fr2) else (fr, fr2 ++ [nx])) ([], [])
